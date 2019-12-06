7e1a936a69d08f5b4959be0b6a325a41b980e8cf
SaucyStew <mtantuico1@ucvts.org>
2019-12-06 10:48:09 -0500

    Made it way better with the pasta

--- bouncy.py
+++ bouncy.py
@@ -4,18 +4,19 @@ import pygame
 # start running pygame
 pygame.init()
 
-width, height = 400, 300
+width, height = 800, 600
 # speed is a list because values need to be able to change
+acceleration = [0, .001]
 speed = [1, 1]
 # colors are tuples because values don't change
-black = (0, 0, 0)
-white = (255, 255, 255)
+black = (112, 83, 34)
+white = (255, 229, 204)
 
 # Create a display. The size must be a tuple providing width and height
 screen = pygame.display.set_mode( (width, height) )
 
 # Load an image
-ball = pygame.image.load("img/ball.png")
+ball = pygame.image.load("img/Pasta.jpg")
 ballrect = ball.get_rect()
 
 while True:
@@ -30,7 +31,8 @@ while True:
         speed[0] *= -1
     if ballrect.top < 0 or ballrect.bottom > height:
         speed[1] *= -1
-
+        # speed[0] += acceleration[0]
+        # speed[1] += acceleration[1]
     # Fill the screen with white
     screen.fill(white)
     # Draw the ball with the size and location specified by ballrect
